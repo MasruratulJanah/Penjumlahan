b321280602347ca477f9df2776ff5a14787532f1
MasruratulJanah <janahmasruratul@gmail.com>
2024-09-16 10:08:31 +0700

    Add files via upload

---
+++ InputPenjumlahan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class InputPenjumlahan {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int angka1, angka2, hasil;
+
+        System.out.print("Bilangan 1 : ");
+        angka1 = in.nextInt();
+
+        System.out.print("Bilangan 2 : ");
+        angka2 = in.nextInt();
+
+        hasil = angka1 + angka2;
+        System.out.println("Hasil Penjumlahan : " + hasil);
+
+    }
+}
